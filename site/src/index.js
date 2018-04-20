02d2b2628d70d3a8854d6079cc774b064cd15621
jennie-quinton <j.quinton1@gmail.com>
2018-04-20 13:23:08 +0100

    react and sass set up

---
+++ site/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Root(){
+    return (
+        <h1> Hello Dnd Master </h1>
+    );
+}
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
