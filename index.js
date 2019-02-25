9a374fc3760d945ddb96a39f9b5a71a55b62584a
jordanrios94 <jordanrios.software@gmail.com>
2019-02-25 22:18:47 +0000

    feat: database, server, and backend scaffold set up

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
+require('./models/Character');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
+const app = express();
+
+app.use(bodyParser.json());
+
+require('./routes/characterRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // TODO: Jen tell me where are you compiling this into
+  // Express will serve up production assets
+  // like our main.js file, main.css file
+  // app.use(express.static('client/build'));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  // const path = require('path');
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
