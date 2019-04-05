60d9a3de744895f2b2ff9ef8ca7d69d8d188ad1f
jennie-quinton <j.quinton1@gmail.com>
2019-04-05 15:13:49 +0100

    more styling

--- client/components/menuBar/index.js
+++ client/components/menuBar/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import styles from './styles.scss';
+import { Heading } from '../basics';
 
 const MenuBar = () => (
   <div className={styles.fullWidth}>
-    <h1>DnD Master</h1>
+    <Heading level={1}>DnD Master</Heading>
   </div>
 );
 
