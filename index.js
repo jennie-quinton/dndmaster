93860ed44e66ca8a42898f11c969270a449dd30b
jennie-quinton <j.quinton1@gmail.com>
2019-04-12 16:51:55 +0100

    trying to deploy

--- index.js
+++ index.js
@@ -57,3 +57,4 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 console.log('LISTENING TO PORT:', PORT);
+console.log('NODE_ENV:', process.env.NODE_ENV);
