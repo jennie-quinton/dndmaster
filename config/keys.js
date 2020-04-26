07d537b0045b15d86b0d5ac31d3b7397da5eaa44
jordanrios94 <jordanrios.software@gmail.com>
2020-04-26 19:06:09 +0100

    chore: added staging keys to be used on staging environment

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
   module.exports = require('./prod');
+} else if (process.env.NODE_ENV === 'staging') {
+  // we are in staging - return the staging set of keys
+  module.exports = require('./staging');
 } else {
   // we are in development - return the dev keys
   module.exports = require('./dev');
