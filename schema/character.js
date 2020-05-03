f066ddb902b884248d31fec5bc77dd3e01296f16
jordanrios94 <jordanrios.software@gmail.com>
2020-05-03 21:07:40 +0100

    feat: added enum type to make it easier to know what values will be stored for race and class

--- schema/character.js
+++ schema/character.js
@@ -6,17 +6,63 @@ const {
   GraphQLList,
   GraphQLNonNull,
   GraphQLID,
+  GraphQLEnumType,
 } = graphql;
 const { GraphQLDateTime } = graphqlIsoDate;
 const CharacterController = require('../controllers/CharacterController');
+
+const RaceEnumType = new GraphQLEnumType({
+  name: 'Race',
+  description: `Every character belongs to a race,
+    one of the many humanoid species in D&D.
+    The most common player character races are dwarves,
+    elves, halflings, and humans.
+    Your character’s race grants particular racial traits,
+    such as special senses.`,
+  values: {
+    dragonborn: { value: 'dragonborn' },
+    dwarf: { value: 'dwarf' },
+    elf: { value: 'elf' },
+    gnome: { value: 'gnome' },
+    halfElf: { value: 'halfElf' },
+    halfling: { value: 'halfling' },
+    halfOrc: { value: 'halfOrc' },
+    human: { value: 'human' },
+    tiefling: { value: 'tiefling' },
+  },
+});
+
+const ClassEnumType = new GraphQLEnumType({
+  name: 'Class',
+  description: `Every adventurer is a member of a character class.
+    Class broadly describes a character’s vocation special
+    talents, and the tactics the character is most likely
+    to employ when exploring a dungeon, fighting monsters,
+    or engaging in a tense negotiation. Your character receives
+    many benefits from your choice of class.`,
+  values: {
+    barbarian: { value: 'barbarian' },
+    bard: { value: 'bard' },
+    cleric: { value: 'cleric' },
+    druid: { value: 'druid' },
+    fighter: { value: 'fighter' },
+    monk: { value: 'monk' },
+    paladin: { value: 'paladin' },
+    ranger: { value: 'ranger' },
+    rogue: { value: 'rogue' },
+    sorcerer: { value: 'sorcerer' },
+    warlock: { value: 'warlock' },
+    wizard: { value: 'wizard' },
+  },
+});
 
 const CharacterType = new GraphQLObjectType({
   name: 'Character',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    race: { type: GraphQLString },
-    class: { type: GraphQLString },
+    race: { type: RaceEnumType },
+    class: { type: ClassEnumType },
     createdAt: { type: GraphQLDateTime },
     updatedAt: { type: GraphQLDateTime },
   }),
@@ -25,7 +71,6 @@ const CharacterType = new GraphQLObjectType({
 const queries = {
   characters: {
     type: new GraphQLList(CharacterType),
-    args: {},
     resolve(parentValue, args, req) {
       return CharacterController.getAllByUserId(req.user.id);
     },
@@ -37,8 +82,8 @@ const mutations = {
     type: CharacterType,
     args: {
       name: { type: new GraphQLNonNull(GraphQLString) },
-      race: { type: new GraphQLNonNull(GraphQLString) },
-      class: { type: new GraphQLNonNull(GraphQLString) },
+      race: { type: new GraphQLNonNull(RaceEnumType) },
+      class: { type: new GraphQLNonNull(ClassEnumType) },
     },
     resolve(parentValue, { name, race, class: className }, req) {
       return CharacterController.create({ name, race, className }, req.user.id);
